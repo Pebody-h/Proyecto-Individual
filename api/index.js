cfe76c0d884799b66aa66409d4446afe731a7ff2
Heider <heiderlopez123@gmail.com>
2022-07-22 23:27:19 -0500

    db en false

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const {
 } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: falce }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
